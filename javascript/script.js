fa1cb78fe36436a5c0825fd35f2c64e6be2a37bf
Miguel Angel <x22116133@student.ncirl.ie>
2022-11-19 14:51:41 +0000

    Changes to IF logic
    Changes to IF logic to make it tidier.
    Will keep thinking about it.
    
    Adding Flex to the site.
    Might add a hide property later on.

--- javascript/script.js
+++ javascript/script.js
@@ -18,15 +18,7 @@ function game()
 	inputPlayer1 = prompt (player1 + " enter your choice of rock, scissors or paper.");
 	inputPlayer2 = prompt (player2 + " enter your choice of rock, scissors or paper.");
 	
-		if (inputPlayer1 == "rock" && inputPlayer2 == "rock")
-		{
-			alert (player1 + " and " + player2 + " chose the same, this is a tie");
-		}
-		else if (inputPlayer1 == "paper" && inputPlayer2 == "paper")
-		{
-			alert (player1 + " and " + player2 + " chose the same, this is a tie");
-		}
-		else if (inputPlayer1 == "scissors" && inputPlayer2 == "scissors")
+		if (inputPlayer1 == inputPlayer2)
 		{
 			alert (player1 + " and " + player2 + " chose the same, this is a tie");
 		}
@@ -51,11 +43,10 @@ function game()
 }			
 		
 		
+/*I wanted to make the code cleaner than all the IF and IF ELSES. 
+I tried first to say 		
 		
-		
-		
-		
-		/*if (inputPlayer1 && inputPlayer2 === "rock" && "rock" || "scissors" && "scissors" || "paper" && "paper"/*&& inputPlayer2 == "rock")
+		*if (inputPlayer1 && inputPlayer2 === "rock" && "rock" || "scissors" && "scissors" || "paper" && "paper"/*&& inputPlayer2 == "rock")
 		{
 			alert (player1 + " and " + player2 + " chose the same, this is a tie");
 		}
@@ -75,4 +66,13 @@ function game()
 		{
 			alert (player1 + " wins");
 		}
-		*/
+but it didn't work because it was always defaulting to "SOMETHING ELSE". 
+
+So I decided to code it with all the IF and ELSE IF because I knew that it was going to work that way. 
+I asked around and somebody mentioned that it was the INPUT that the players were entering the one that had to be the same for it to be a tie. 
+I didn't get it because I thought that it meant the words I had. 
+But no!, it was the inputPlayer1 and inputPlayer2!! and it makes sense because that variable is the one that we are declaring and the one
+that the program is storing.
+
+I am updating the program with that line and I will keep thinking about how to make the other IFS cleaner.
+*/
